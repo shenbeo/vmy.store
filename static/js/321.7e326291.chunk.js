1f684e15248d86f7ef50342f0660ff8ea90e0b54
shenbeo <vinhnbt26@gmail.com>
2023-05-12 01:25:05 +0700

    Updates

---
+++ static/js/321.7e326291.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvmy_store=self.webpackChunkvmy_store||[]).push([[321],{4321:function(e,t,s){s.r(t),s.d(t,{default:function(){return r}});s(2791);var c=s(184);function r(){return(0,c.jsx)("div",{className:"flex items-center justify-center w-full h-full",children:(0,c.jsx)("img",{className:"w-[60px] object-cover p-2 m-2",src:"https://i.gifer.com/ZZ5H.gif",alt:""})})}}}]);
+//# sourceMappingURL=321.7e326291.chunk.js.map
